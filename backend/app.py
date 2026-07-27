f8f6631ec329f43b6eeecad0d93d458bd252f1e3
shashank_naik-admin <mrthreesixty360iu@gmail.com>
2026-07-27 17:35:33 +0530

    feat:create users table

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from app.config.database import get_connection
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Employee Portal Backend Running"
+    try:
+        conn = get_connection()
+        conn.close()
+        return "Database Connected Successfully!"
+    except Exception as e:
+        return f"Database Connection Failed: {e}"
 
 if __name__ == "__main__":
     app.run(debug=True)
